3224dbff03a09190bf516b37ab4c2f72a625345c
michael.grinfeld <michael.grinfeld@dynamicyield.com>
2021-02-21 10:16:28 +0200

    small refactor to ensure no exceptions (in case value is null)

--- src/main/scala/com/mikerusoft/citizens/data/readers/csv/HeaderConverter.scala
+++ src/main/scala/com/mikerusoft/citizens/data/readers/csv/HeaderConverter.scala
@@ -15,7 +15,7 @@ object HeaderConverter {
 
   implicit object RemoveConverter extends HeaderConverter[Remove] {
     override def convert(header: Remove, value: String, builder: Person.Builder): Person.Builder = {
-      Option(value).tryIt(s => if (s.isBlank || s.equals("") || !s.equalsIgnoreCase("true") || s.equals("0")) false else true)
+      Option(value).trim().tryIt(s => if (s.isBlank || s.equals("") || !s.equalsIgnoreCase("true") || s.equals("0")) false else true)
         .toBuilder(builder.withRemove)(builder)
     }
   }
@@ -26,23 +26,23 @@ object HeaderConverter {
     private def appendWithZeros(value: String): String = if (value.length >= 9) value else appendWithZeros("0" + value)
 
     override def convert(header: Tz, value: String, builder: Person.Builder): Person.Builder = {
-      Option(value).tryIt(_.toLong.toString).filter(v => v.length <= 9).map(appendWithZeros).toBuilder(builder.withTz)(builder)
+      Option(value).trim().tryIt(_.toLong.toString).filter(v => v.length <= 9).map(appendWithZeros).toBuilder(builder.withTz)(builder)
     }
   }
 
   implicit object FirstNameConverter extends HeaderConverter[FirstName] {
     override def convert(header: FirstName, value: String, builder: Person.Builder): Person.Builder =
-      Option(value).toBuilder(v => builder.withPersonalInfo(b => b.withFirstName(v)))(builder)
+      Option(value).trim().toBuilder(v => builder.withPersonalInfo(b => b.withFirstName(v)))(builder)
   }
 
   implicit object LastNameConverter extends HeaderConverter[LastName] {
     override def convert(header: LastName, value: String, builder: Person.Builder): Person.Builder =
-      Option(value).toBuilder(v => builder.withPersonalInfo(b => b.withLastName(v)))(builder)
+      Option(value).trim().toBuilder(v => builder.withPersonalInfo(b => b.withLastName(v)))(builder)
   }
 
   implicit object MiddleNameConverter extends HeaderConverter[MiddleName] {
     override def convert(header: MiddleName, value: String, builder: Person.Builder): Person.Builder =
-      Option(value).toBuilder(v => builder.withPersonalInfo(b => b.withMiddleName(v)))(builder)
+      Option(value).trim().toBuilder(v => builder.withPersonalInfo(b => b.withMiddleName(v)))(builder)
   }
 
   private def fillFirstAndLastName(builder: Person.Builder, bs: PersonalInfo.Builder): Person.Builder = {
@@ -52,9 +52,9 @@ object HeaderConverter {
 
   implicit object FullNameFirstNameFirstConverter extends HeaderConverter[FullNameFirstNameFirst] {
     override def convert(header: FullNameFirstNameFirst, value: String, builder: Person.Builder): Person.Builder = {
-      Option(value).tryIt(_.split(header.delimiter).toList)
-        .toBuilder(v => {
-          fillFirstAndLastName(builder, v match {
+      Option(value).trim().tryIt(_.split(header.delimiter).toList)
+        .toBuilder(names => {
+          fillFirstAndLastName(builder, names match {
             case Nil => PersonalInfo.builder()
             case header :: Nil => PersonalInfo.builder().withLastName(header)
             case header :: remainder => PersonalInfo.builder().withFirstName(header).withLastName(remainder.mkString(" "))
@@ -65,9 +65,9 @@ object HeaderConverter {
 
   implicit object FullNameLastNameFirstConverter extends HeaderConverter[FullNameLastNameFirst] {
     override def convert(header: FullNameLastNameFirst, value: String, builder: Person.Builder): Person.Builder = {
-      Option(value).tryIt(_.split(header.delimiter).toList)
-        .toBuilder(v => {
-          fillFirstAndLastName(builder, v match {
+      Option(value).trim().tryIt(_.split(header.delimiter).toList)
+        .toBuilder(names => {
+          fillFirstAndLastName(builder, names match {
             case Nil => PersonalInfo.builder()
             case header :: Nil => PersonalInfo.builder().withLastName(header)
             case header :: remainder => PersonalInfo.builder().withLastName(header).withFirstName(remainder.mkString(" "))
@@ -78,64 +78,64 @@ object HeaderConverter {
 
   implicit object AgeConverter extends HeaderConverter[Age] {
     override def convert(header: Age, value: String, builder: Person.Builder): Person.Builder = {
-      Option(value).tryIt(_.toInt).map(age => LocalDateTime.now().minus(age, ChronoUnit.YEARS))
+      Option(value).trim().tryIt(_.toInt).map(age => LocalDateTime.now().minus(age, ChronoUnit.YEARS))
         .map(dt => dt.get(ChronoField.YEAR)).filter(t => t > 1900).toBuilder(v => builder.withPersonalInfo(_.bornYear(v)))(builder)
     }
   }
 
   implicit object BornYearConverter extends HeaderConverter[BornYear] {
-    override def convert(header: BornYear, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: BornYear, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .tryIt(_.toInt).toBuilder(v => builder.withPersonalInfo(_.bornYear(v)))(builder)
   }
 
   implicit object BirthdayConverter extends HeaderConverter[BirthDay] {
-    override def convert(header: BirthDay, value: String, builder: Person.Builder): Person.Builder = Option(value).map(_.trim)
+    override def convert(header: BirthDay, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .tryIt(date => header.dateFormat.parse(date)).map(dt => dt.get(ChronoField.YEAR)).filter(t => t > 1900)
       .toBuilder(v => builder.withPersonalInfo(_.bornYear(v)))(builder)
   }
 
   implicit object CityConverter extends HeaderConverter[City] {
-    override def convert(header: City, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: City, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.city(v)))(builder)
   }
 
   implicit object StreetConverter extends HeaderConverter[Street] {
-    override def convert(header: Street, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: Street, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.street(v)))(builder)
   }
 
   implicit object BuildingNoConverter extends HeaderConverter[BuildingNo] {
-    override def convert(header: BuildingNo, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: BuildingNo, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.buildingNo(v)))(builder)
   }
 
   implicit object ApartmentNoConverter extends HeaderConverter[ApartmentNo] {
-    override def convert(header: ApartmentNo, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: ApartmentNo, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.apartment(v)))(builder)
   }
 
   implicit object EntranceConverter extends HeaderConverter[Entrance] {
-    override def convert(header: Entrance, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: Entrance, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.entrance(v)))(builder)
   }
 
   implicit object NeighborhoodNameConverter extends HeaderConverter[NeighborhoodName] {
-    override def convert(header: NeighborhoodName, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: NeighborhoodName, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withAddress(_.neighborhood(v)))(builder)
   }
 
   implicit object EmailConverter extends HeaderConverter[Email] {
-    override def convert(header: Email, value: String, builder: Person.Builder): Person.Builder = Option(value)
+    override def convert(header: Email, value: String, builder: Person.Builder): Person.Builder = Option(value).trim()
       .toBuilder(v => builder.withEmails(v :: builder.emails))(builder)
   }
 
   implicit object TagsConverter extends HeaderConverter[Tags] {
     override def convert(header: Tags, value: String, builder: Person.Builder): Person.Builder =
-      Option(value).tryIt(_.split(header.delimiter).toList) .toBuilder(builder.withTags)(builder)
+      Option(value).trim().tryIt(_.split(header.delimiter).toList) .toBuilder(builder.withTags)(builder)
   }
 
   private def parsePhone(value: String, header: PhoneNumberHeader, builder: Person.Builder): Person.Builder = {
-    (Option(value).tryIt(_.replaceAll("[\\s\\-\\(\\)]", "").toLong.toString) match {
+    (Option(value).trim().tryIt(_.replaceAll("[\\s\\-\\(\\)]", "").toLong.toString) match {
       case Some(phoneStringValue) =>
         val phoneNum = if (phoneStringValue.startsWith(header.countryToUse)) {
           phoneStringValue
@@ -172,6 +172,7 @@ object HeaderConverter {
         case Failure(_) => None
       })
     }
+    def trim():Option[String] = value.map(_.trim)
   }
 
   implicit class OptToBuilder[T](value: Option[T]) {
